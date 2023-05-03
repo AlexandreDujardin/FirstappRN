2f1b7120ad2cd1c2a2dd343aa5c35373c56df4fa
Marius <marius.sergent@gmail.com>
2023-05-03 14:39:38 +0200

    remove eslint, add standard

---
+++ App.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Text,
+  useColorScheme,
+  View
+} from 'react-native'
+
+import {
+  Colors
+} from 'react-native/Libraries/NewAppScreen'
+
+function App () {
+  const isDarkMode = useColorScheme() === 'dark'
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter
+  }
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior='automatic'
+        style={backgroundStyle}
+      >
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white
+          }}
+        >
+          <Text style={{ color: 'black' }}>COUCOU</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default App
